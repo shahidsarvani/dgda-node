aacdefa02e508d40fa6f6949394da5232d3d9302
HamzaZafarBhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-07-04 14:27:44 +0500

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,32 +1,32 @@
 const net = require('net');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-var server = net.createServer();
+// const mysql = require('mysql');
+// var server = net.createServer();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', /* MySQL User */
-    password: '', /* MySQL Password */
-    database: 'dgda' /* MySQL Database */
-    // host: '18.170.155.197',
-    // user: 'admin_dgda_cms_user', /* MySQL User */
-    // password: '3S~9f7a7b', /* MySQL Password */
-    // database: 'admin_dgda_cms_db' /* MySQL Database */
-});
+// const conn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root', /* MySQL User */
+//     password: '', /* MySQL Password */
+//     database: 'dgda' /* MySQL Database */
+//     // host: '18.170.155.197',
+//     // user: 'admin_dgda_cms_user', /* MySQL User */
+//     // password: '3S~9f7a7b', /* MySQL Password */
+//     // database: 'admin_dgda_cms_db' /* MySQL Database */
+// });
 
-conn.connect((err) => {
-    if (err) throw err;
-    console.log('Mysql Connected with App...');
-});
+// conn.connect((err) => {
+//     if (err) throw err;
+//     console.log('Mysql Connected with App...');
+// });
 
-server.listen(58900, () => {
-    console.log('opened server on %j', server.address().port);
-});
-const client = server.createConnection({ port: 58900 }, () => {
+// server.listen(58900, () => {
+//     console.log('opened server on %j', server.address().port);
+// });
+const client = net.createConnection({ port: 58900 }, () => {
     // 'connect' listener.
     console.log('connected to server!');
     console.log("Client connection details - ", client.remoteAddress + ":" + client.remotePort);
