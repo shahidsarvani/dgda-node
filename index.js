aaf77e446ac0e707494c40467daec16eb7a8e604
HamzaZafarBhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-07-23 13:25:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -800,6 +800,101 @@ app.post('/api/zone/:id/play_scene', (req, res) => {
     });
 })
 
+app.get('/api/room/:id/get_play_wall_video', (req, res) => {
+    try {
+        let sqlQuery = "SELECT id, title_en FROM `wall_media` WHERE room_id = " + req.params.id;
+
+        let query = conn.query(sqlQuery, (err, results) => {
+            if (err) {
+                return res.send(apiResponseBad(null));
+            }
+            results.map(function (result) {
+                result.title = result.title_en
+            })
+            res.send(apiResponse(results));
+        });
+    } catch (error) {
+        console.log(error)
+        res.send(apiResponseBad(null));
+    }
+});
+
+app.get('/api/room/:id/get_play_wall_video/ar', (req, res) => {
+    try {
+        let sqlQuery = "SELECT id, title_ar FROM `wall_media` WHERE room_id = " + req.params.id;
+
+        let query = conn.query(sqlQuery, (err, scenes) => {
+            if (err) {
+                return res.send(apiResponseBad(null));
+            };
+            scenes.map(function (result) {
+                result.title = result.title_ar
+            })
+            res.send(apiResponse(scenes));
+        });
+    } catch (error) {
+        console.log(error)
+        res.send(apiResponseBad(null));
+    }
+});
+// app.get('/api/room/:id/get_play_wall_video', (req, res) => {
+//     let sqlQuery = "SELECT id, title_en FROM `wall_media` WHERE room_id = " + req.params.id ;
+
+//     // return res.send(apiResponse(sqlQuery2));
+//     if (process.env.APP_ENV == 'prod') {
+//         let query2 = conn.query(sqlQuery2, (err, results) => {
+//             if (err) {
+//                 res.send(apiResponseBad(null));
+//             } else {
+//                 var execCommands = async () => {
+//                     await sendCrestCommands(results);
+//                     await sendModelCommands(results);
+//                 };
+//                 execCommands();
+//             }
+//         });
+//     }
+//     let query = conn.query(sqlQuery, (err, results) => {
+//         if (err) {
+//             return res.send(apiResponseBad(null));
+//         };
+//         // return res.send(apiResponse(results));
+//         var p_video = '';
+//         var duration = 0;
+//         for (var i = 0; i < results.length; i++) {
+//             if (results[i].is_projector) {
+//                 p_video = [
+//                     encodeURI(process.env.PROD_VIDEO_PATH + results[i].name),
+//                     results[i].is_image,
+//                 ]
+//                 break;
+//             }
+//         }
+//         var w_video = '';
+//         for (var i = 0; i < results.length; i++) {
+//             if (!results[i].is_projector) {
+//                 w_video = [
+//                     encodeURI(process.env.PROD_VIDEO_PATH + results[i].name),
+//                     lang
+//                 ]
+//                 duration = results[i].duration
+//                 break;
+//             }
+//         }
+//         // return res.send(apiResponse(w_video));
+//         if (req.params.id == process.env.WS_ID) {
+//             io.emit('change_video_wsw', w_video);
+//             io.emit('change_video_wsp', p_video);
+//         } else {
+//             io.emit('change_video_dw', w_video);
+//             io.emit('change_video_dp', p_video);
+//         }
+//         // io.emit('change_video', w_video);
+//         // io.emit('change_video_p', p_video);
+//         return res.send(apiResponse(duration));
+//     });
+// })
+
 app.get('/api/test', (req, res) => {
     const child_argv = [
         'test',
